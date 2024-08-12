b511e0b0cd341cb2035875fc618d74e45d8028f3
Nicolly18 <135607022+Nicolly18@users.noreply.github.com>
2024-08-12 23:09:28 +0000

    12/08/2024

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const idade =18;
+let idade =18;
 const anoNascimento = 2006;
 let anoAtual = 2024;
+const nomeCompleto = "Nicolly Teixeira";
+
+console.log("meu nome é " + nomeCompleto + " nascido no ano de   " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
-
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
+
+
+
